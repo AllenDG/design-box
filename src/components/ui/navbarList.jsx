11a7a247bf0a88f8afeca47d5521e4c6c14ca366
allenDG <allenwalterfiestada@gmail.com>
2024-12-07 14:22:21 +0800

    update

--- src/components/ui/navbarList.jsx
+++ src/components/ui/navbarList.jsx
@@ -1,5 +1,5 @@
 import { navBarRoutes } from "@/routes/navbarRoutes";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 
 const NavbarList = () => {
   const location = useLocation();
@@ -10,15 +10,22 @@ const NavbarList = () => {
         const isActive = location.pathname === route.path;
         return (
           <li key={route.path} className="group">
-            <a
-              href={route.path}
-              className={`relative rounded px-4 py-2 text-sm font-medium transition duration-300 ${isActive ? "text-secondary-400" : "text-secondary-800"} hover:text-secondary-300`}
+            {/* Use Link for internal navigation */}
+            <Link
+              to={route.path}
+              className={`relative rounded px-4 py-2 text-sm font-medium transition duration-300 ${
+                isActive ? "text-secondary-400" : "text-secondary-800"
+              } hover:text-secondary-300`}
             >
               {route.label}
               <span
-                className={`absolute bottom-0 left-0 h-[2px] transition-all duration-300 ${isActive ? "bg-secondary-400 w-full" : "bg-secondary-400 w-0 group-hover:w-full"}`}
+                className={`absolute bottom-0 left-0 h-[2px] transition-all duration-300 ${
+                  isActive
+                    ? "bg-secondary-400 w-full"
+                    : "bg-secondary-400 w-0 group-hover:w-full"
+                }`}
               />
-            </a>
+            </Link>
           </li>
         );
       })}
